657b8950c629480ece5006d53f7bdf30f1a579ed
Fred-Lyu <fredlyu0924@gmail.com>
2024-02-04 17:34:05 +0800

    update

---
+++ cuda_rasterizer/rasterizer_impl.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2023, Inria
+ * GRAPHDECO research group, https://team.inria.fr/graphdeco
+ * All rights reserved.
+ *
+ * This software is free for non-commercial, research and evaluation use 
+ * under the terms of the LICENSE.md file.
+ *
+ * For inquiries contact  george.drettakis@inria.fr
+ */
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include "rasterizer.h"
+#include <cuda_runtime_api.h>
+
+namespace CudaRasterizer
+{
+	template <typename T>
+	static void obtain(char*& chunk, T*& ptr, std::size_t count, std::size_t alignment)
+	{
+		std::size_t offset = (reinterpret_cast<std::uintptr_t>(chunk) + alignment - 1) & ~(alignment - 1);
+		ptr = reinterpret_cast<T*>(offset);
+		chunk = reinterpret_cast<char*>(ptr + count);
+	}
+
+    // 高斯点状态类，长度为此时高斯点数量
+	struct GeometryState
+	{
+		size_t scan_size;
+		float* depths;
+		char* scanning_space;
+		bool* clamped;
+		int* internal_radii;
+		float2* means2D;
+		float* cov3D;
+		float4* conic_opacity;
+		float* rgb;
+		uint32_t* point_offsets;
+		uint32_t* tiles_touched;
+
+		static GeometryState fromChunk(char*& chunk, size_t P);
+	};
+
+    // 图像类
+	struct ImageState
+	{
+		uint2* ranges;          // 长度：tile内图像像素的个数   uint2.x：每个tile涉及到点的起始位置 uint2.y：每个tile涉及到点的终止位置 注：范围左闭右开[)
+		uint32_t* n_contrib;    // 长度：图像像素的个数       该像素被多少个高斯点渲染
+		float* accum_alpha;     // 长度：图像像素的个数       该像素积累的alpha不透明度值
+
+		static ImageState fromChunk(char*& chunk, size_t N);
+	};
+
+    // 排序类，按tile和每tile内高斯点深度排序存储
+	struct BinningState
+	{
+		size_t sorting_size;
+		uint64_t* point_list_keys_unsorted;
+		uint64_t* point_list_keys;
+		uint32_t* point_list_unsorted;
+		uint32_t* point_list;
+		char* list_sorting_space;
+
+		static BinningState fromChunk(char*& chunk, size_t P);
+	};
+
+	template<typename T> 
+	size_t required(size_t P)
+	{
+		char* size = nullptr;
+		T::fromChunk(size, P);
+		return ((size_t)size) + 128;
+	}
+};
